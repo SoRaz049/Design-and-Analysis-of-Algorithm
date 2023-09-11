1210bb8333ddc3c111727b4916d806856b9981ec
Swaraj Barnwal <ikku12piku@gmail.com>
2023-09-11 08:10:04 +0545

    Still some errors to fix

---
+++ quick_merge_sort.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+int counter =0;
+
+void Sorted_array(int A[], int n)
+{
+    int i,seperator;
+    string sep;
+    cout<<"The sorted array is:"<<endl;
+    cout<<"| ";
+    seperator = n;
+    sep = string(seperator,'|');
+    for(i=0; i<n ; i++)
+    {
+        cout<<" "<<sep;
+    }
+}
+int Quick_Partition(int A[], int l, int r)
+{
+    int pivot = A[r], i = l-1;
+    int j;
+
+    for(j=l; j<r; j++)
+    {
+        if(A[j]<pivot)
+        {
+            i++;
+            swap(A[i],A[j]);
+        }
+
+    }
+
+    swap(A[i+1],r);
+    return i+1;
+}
+
+int Quick_Sort(int A[], int l, int r)
+{
+    int p;
+    if(l <= r)
+    {
+        counter++;
+        p = Quick_Partition(A,l,r);
+        Quick_Sort(A, l, p-1);
+        Quick_Sort(A, p+1, r);
+    }
+    return counter;
+}
+
+
+
+int main()
+{
+    int A[]={4,5,6,8,3,1};
+    int n = 6;
+    int c;
+    c = Quick_Sort(A,0 , n-1);
+    Sorted_array(A,n);
+    cout<<c;
+}
